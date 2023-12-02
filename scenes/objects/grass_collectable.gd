27c50b63db13131bba74591ce8c01f73e3b2541c
mfroemmi <marcel.froemmi@gmail.com>
2023-12-02 16:34:07 +0100

    Added timer to collectable grass object

--- scenes/objects/grass_collectable.gd
+++ scenes/objects/grass_collectable.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var meshInstance3D: MeshInstance3D = $Grass_000/MeshInstance3D
 @onready var grass: MeshInstance3D = $Grass_000
+@onready var timer: Timer = $Timer
 
 var isBodyEntered: bool = false
 var isMouseEntered: bool = false
@@ -10,6 +11,8 @@ var isMouseEntered: bool = false
 func _ready():
 	$CollisionArea3D.body_entered.connect(on_body_entered)
 	$CollisionArea3D.body_exited.connect(on_body_exited)
+	
+	timer.timeout.connect(on_timer_timeout)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,6 +34,7 @@ func _on_area_3d_mouse_collision_mouse_exited():
 func _on_area_3d_mouse_collision_input_event(camera, event, position, normal, shape_idx):
 	if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT) and isMouseEntered:
 		grass.visible = false
+		timer.start()
 
 
 func on_body_entered(other_body: Node3D):
@@ -40,3 +44,5 @@ func on_body_entered(other_body: Node3D):
 func on_body_exited(other_body: Node3D):
 	isBodyEntered = false
 
+func on_timer_timeout():
+	grass.visible = true
