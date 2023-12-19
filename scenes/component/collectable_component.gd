481d0db3cc483c605bb952bcc6503eb0a1563e26
mfroemmi <marcel.froemmi@gmail.com>
2023-12-19 20:40:27 +0100

    Finished collectable component

--- scenes/component/collectable_component.gd
+++ scenes/component/collectable_component.gd
@@ -1,35 +1,20 @@
 extends Node3D
 
-@export var meshInstance3D: MeshInstance3D
+@export var outlineMesh: MeshInstance3D
+@export var mouseCollision: CollisionObject3D
+@export var bodyCollision: Area3D
 
 var isBodyEntered: bool = false
 var isMouseEntered: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CollisionArea3D.body_entered.connect(on_body_entered)
-	$CollisionArea3D.body_exited.connect(on_body_exited)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_area_3d_mouse_entered():
-	if isBodyEntered:
-		isMouseEntered = true
-		meshInstance3D.visible = true
-
-
-func _on_area_3d_mouse_exited():
-	isMouseEntered = false
-	meshInstance3D.visible = false
+	bodyCollision.body_entered.connect(on_body_entered)
+	bodyCollision.body_exited.connect(on_body_exited)
 	
-	
-func _on_area_3d_mouse_collision_input_event(camera, event, position, normal, shape_idx):
-	if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT) and isMouseEntered:
-		owner.queue_free()
+	mouseCollision.input_event.connect(on_area_3d_mouse_collision_input_event)
+	mouseCollision.mouse_entered.connect(on_area_3d_mouse_entered)
+	mouseCollision.mouse_exited.connect(on_area_3d_mouse_exited)
 
 
 func on_body_entered(other_body: Node3D):
@@ -38,4 +23,20 @@ func on_body_entered(other_body: Node3D):
 	
 func on_body_exited(other_body: Node3D):
 	isBodyEntered = false
+
+
+func on_area_3d_mouse_entered():
+	if isBodyEntered:
+		isMouseEntered = true
+		outlineMesh.visible = true
+
+
+func on_area_3d_mouse_exited():
+	isMouseEntered = false
+	outlineMesh.visible = false
+	
+	
+func on_area_3d_mouse_collision_input_event(camera, event, position, normal, shape_idx):
+	if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT) and isMouseEntered:
+		owner.queue_free()
 
