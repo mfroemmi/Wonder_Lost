7a527f73973198fcd19cf09e1c1fc63f2c209b1b
mfroemmi <marcel.froemmi@gmail.com>
2023-12-29 16:09:17 +0100

    Worked on the inventory system

--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -2,4 +2,20 @@ extends Resource
 
 class_name InventoryData
 
+signal inventory_updated(inventory_data: InventoryData)
+signal inventory_interact(inventory_data: InventoryData, index: int, button: int)
+
 @export var slot_datas: Array[SlotData]
+
+func grab_slot_data(index: int) -> SlotData:
+	var slot_data = slot_datas[index]
+	
+	if slot_data:
+		slot_datas[index] = null
+		inventory_updated.emit(self)
+		return slot_data
+	else:
+		return null
+
+func on_slot_clicked(index: int, button: int):
+	inventory_interact.emit(self, index, button)
