62fabd49061ee2c1c600d74916b3b280673992a1
mfroemmi <marcel.froemmi@gmail.com>
2024-04-19 17:39:48 +0200

    Fixed bug for collectable items

--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -51,7 +51,7 @@ func drop_single_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
 func pick_up_slot_data(slot_data: SlotData) -> bool:
 	for index in slot_datas.size():
 		if not slot_datas[index]:
-			slot_datas[index] = slot_data
+			slot_datas[index] = slot_data.create_single_slot_data()
 			inventory_updated.emit(self)
 			return true
 			
