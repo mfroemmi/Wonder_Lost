90cdf854aba94bc4586ef6c7ff53132bae3661f7
mfroemmi <marcel.froemmi@gmail.com>
2024-06-17 19:55:19 +0200

    Added banish stone panel

--- inventory/inventory_interface.gd
+++ inventory/inventory_interface.gd
@@ -8,43 +8,57 @@ var external_inventory_owner
 @onready var player_inventory = $PlayerInventory
 @onready var grabbed_slot = $GrabbedSlot
 @onready var external_inventory = $ExternalInventory
+@onready var banish_stone_panel = $BanishStonePanel
 
 func _physics_process(_delta):
 	if grabbed_slot.visible:
 		grabbed_slot.global_position = get_global_mouse_position() + Vector2(5, 5)
 		
 
+# Player-Inventory
 func set_player_inventory_data(inventory_data: InventoryData):
 	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
 	
 
-func set_external_inventory(_external_inventory_owner):
+# External-Inventory (Chest, Banish)
+func set_external_inventory(_external_inventory_owner, inventory_type):
 	external_inventory_owner = _external_inventory_owner
 	var inventory_data = external_inventory_owner.inventory_data
 	
 	inventory_data.inventory_interact.connect(on_inventory_interact)
-	external_inventory.set_inventory_data(inventory_data)
 	
-	external_inventory.show()
+	if inventory_type == INVENTORY_TYPE.CHEST:
+		external_inventory.set_inventory_data(inventory_data)
+		external_inventory.show()
+	if inventory_type == INVENTORY_TYPE.BANISH:
+		banish_stone_panel.set_inventory_data(inventory_data)
+		banish_stone_panel.show()
 	
 	
-func clear_external_inventory():
+func clear_external_inventory(inventory_type):
 	if external_inventory_owner:
 		var inventory_data = external_inventory_owner.inventory_data
 		
 		inventory_data.inventory_interact.disconnect(on_inventory_interact)
-		external_inventory.clear_inventory_data(inventory_data)
 		
-		external_inventory.hide()
-		external_inventory_owner = null
+		if inventory_type == INVENTORY_TYPE.CHEST:
+			external_inventory.clear_inventory_data(inventory_data)
+			external_inventory.hide()
+			external_inventory_owner = null
+		if inventory_type == INVENTORY_TYPE.BANISH:
+			banish_stone_panel.clear_inventory_data(inventory_data)
+			banish_stone_panel.hide()
+			external_inventory_owner = null
+			
 	
 func is_external_inventory_open() -> bool:
 	if external_inventory_owner:
 		return true
 	else:
 		return false
-	
+
+# Interact
 func on_inventory_interact(inventory_data: InventoryData, index: int, button: int):
 	match [grabbed_slot_data, button]:
 		[null, MOUSE_BUTTON_LEFT]:
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,11 +3,18 @@ extends Node3D
 @onready var stone_buddy = $EntitiesLayer/StoneBuddy
 @onready var inventory_interface = $UI/InventoryInterface
 
+var inventory_type = INVENTORY_TYPE.NONE
+
 func _ready():
 	stone_buddy.toggle_inventory.connect(on_toggle_inventory_interface)
 	inventory_interface.set_player_inventory_data(stone_buddy.inventory_data)
 	
 	for node in get_tree().get_nodes_in_group("external_inventory"):
+		inventory_type = INVENTORY_TYPE.CHEST
+		node.toggle_inventory.connect(on_toggle_inventory_interface)
+		
+	for node in get_tree().get_nodes_in_group("banish_stone_panel"):
+		inventory_type = INVENTORY_TYPE.BANISH
 		node.toggle_inventory.connect(on_toggle_inventory_interface)
 
 
@@ -20,10 +27,10 @@ func on_toggle_inventory_interface(external_inventory_owner = null):
 	if external_inventory_owner:
 		if is_external_inventory_open == false:
 			inventory_interface.visible = true
-			inventory_interface.set_external_inventory(external_inventory_owner)
+			inventory_interface.set_external_inventory(external_inventory_owner, inventory_type)
 		else:
 			inventory_interface.visible = false
-			inventory_interface.clear_external_inventory()
+			inventory_interface.clear_external_inventory(inventory_type)
 
 
 func _on_inventory_interface_drop_slot_data(slot_data: SlotData):
